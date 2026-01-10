940b29c361a07ff9c28c46dacc5d3cd9f8f3ad09
Aakash <gangwani.aakash30@gmail.com>
2026-01-10 11:27:39 +0530

    prisma library added

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,9 @@ const app=express();
 app.use(express.json());
 app.use(morgan('dev'));
 
-
+app.use('*',(req,res,next)=>{
+    return next(new APPError(`can't find ${req.originalUrl} on this server!`,404));
+})
 
 
 app.use(errorHandler);
